826973e298c1a03853598b1e1acf425979811c96
Gustavo Magalhães <glmagalhaes@hotmail.com>
2013-09-12 21:50:06 -0300

    BUG FIXES and improvements
    The bug where it wouldn't download the first required comic is fixed, as
    well as some improvements in the download code.
    It isn't necessary to explicity say that it is the first time you use
    it, when updating, if it finds an empty database it start from the first
    comic.

--- src/main.rb
+++ src/main.rb
@@ -5,32 +5,44 @@ require 'timeout'
 
 #this will be the code to download all the old comics
 #it returns the next comic url if available
-def download(site="http://xkcd.com/1")
-	src_comic = ''
-	alt_text = ''
+def download(site)
 	begin
 		Timeout::timeout(60){	
+			src_comic = ''
+			alt_text = ''
+			
+			#Download the web Page
 			comic = Nokogiri::HTML(open(site))
+			
+			#Get the comic Title
 			title = comic.xpath("//div[@id='ctitle']").text
+			
+			#Download the comic itself
 			comic.xpath("//div[@id='comic']").css('img').map{ |i|
 				open('./xkcd_comics/'<< File.basename(i['src']), 'wb') do |file|
 					file << open(i['src']).read
 				end
+				#get the filename of the comic
 				src_comic =  File.basename(i['src'])
+				#get the alt-text of the comic
 				alt_text =  i['title']
 			}
+			
+			#Get the comic unique id
+			comic_id = URI(site).path.split('/').join.to_i
+
+			#Insert the comic in the database
+			$comics.insert(:id => comic_id , :title => title, :file => src_comic, :alt => alt_text)
+			print "Downloaded comic #%s \n" % comic_id
 
 			comic.xpath("//ul[@class='comicNav']").css('a').map{ |i|
 				if i['rel'] == "next"
 					if i['href'] == '#'
 						print 'Finished Download'
 						return ''
+					else
+						return 'http://xkcd.com' << i['href']
 					end
-
-					$comics.insert(:id => i['href'].split('/').join.to_i , :title => title, :file => src_comic, :alt => alt_text)
-					print "Downloaded comic #%s \n" % i['href'].split('/').join
-					return 'http://xkcd.com' << i['href']
-
 				end
 			}
 		}
@@ -42,28 +54,36 @@ end
 
 #Finds Last downloaded comic and continue from there until it's over
 def update()
-	comic = ''
-	begin
-		Timeout::timeout(5){	
-			comic = Nokogiri::HTML(open('http://xkcd.com/' << $comics.max(:id).to_s << '/' ))
-		}
-	rescue Timeout::Error
-		print  "Error Downloading comic #%s \n" % $comics.max(:id)
-		sleep 0.42 and retry
-	end
-	comic.xpath("//ul[@class='comicNav']").css('a').map{ |i|
-		if i['rel'] == "next"
-			if !(i['href'] == '#')
-				next_comic = "http://xkcd.com" << i['href']
-				begin
-					next_comic = download(next_comic)
-				end while next_comic != ""
-			else
-				print "Finished Updating Comics!\n"
-			end
-			return
+	if ($comics.max(:id) == nil)
+		next_comic = "http://xkcd.com/1/"
+		begin
+			next_comic = download(next_comic)
+		end while next_comic != ""
+		print "Finished Updating Comics!\n"
+	else
+		comic = ''
+		begin
+			Timeout::timeout(5){	
+				comic = Nokogiri::HTML(open('http://xkcd.com/' << $comics.max(:id).to_s << '/' ))
+			}
+		rescue Timeout::Error
+			print  "Error Downloading comic #%s \n" % $comics.max(:id)
+			sleep 0.42 and retry
 		end
-	}
+		comic.xpath("//ul[@class='comicNav']").css('a').map{ |i|
+			if i['rel'] == "next"
+				if !(i['href'] == '#')
+					next_comic = "http://xkcd.com" << i['href']
+					begin
+						next_comic = download(next_comic)
+					end while next_comic != ""
+				else
+					print "Finished Updating Comics!\n"
+				end
+				return
+			end
+		}
+	end
 end
 
 def info(id)
@@ -99,8 +119,7 @@ end
 exit = false
 while !exit do 
 	print "Menu\n\n"
-	print "1  - Firts Time\n"
-	print "2  - Update\n"
+	print "1  - Update\n"
 	print "3  - Info From Comic #\n"
 	print "99 - Quit\n\n"
 	unselected = true
@@ -108,13 +127,6 @@ while !exit do
 		print "= "
 		option = gets.chomp
 		if '1' == option
-			unselected = false
-			next_comic = "http://xkcd.com/1/"
-			begin
-				next_comic = download(next_comic)
-			end while next_comic != ""
-		end
-		if '2' == option
 			unselected = false
 			update()
 		end
